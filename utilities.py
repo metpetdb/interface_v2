657f069b2940fbd51409ce7ca77668d1b405717c
Krishna Aradhi <krishna@aradhi.me>
2014-05-30 15:44:38 -0400

    Minor changes to the utilities file

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 from flask import url_for
+from urllib import urlencode
 
 def paginate_model(model_name, data, filters):
     new_filters = dict(filters)
@@ -9,18 +10,18 @@ def paginate_model(model_name, data, filters):
         next_offset = int(filters['offset']) + 20
         next = url_for(model_name) + '?' + 'offset={0}'.format(next_offset)
         if new_filters:
-            next = next + '&' + urllib.urlencode(new_filters)
+            next = next + '&' + urlencode(new_filters)
 
     if data.data['meta']['previous']:
         prev_offset = int(filters['offset']) - 20
         previous = url_for(model_name) + '?' + 'offset={0}'.format(prev_offset)
         if new_filters:
-            previous = previous + '&' + urllib.urlencode(new_filters)
+            previous = previous + '&' + urlencode(new_filters)
 
     total_count = data.data['meta']['total_count']
     last = url_for(model_name) + '?' + 'offset={0}'.format(\
                   total_count - total_count%20)
     if new_filters:
-        last = last + '&' + urllib.urlencode(new_filters)
+        last = last + '&' + urlencode(new_filters)
 
     return (next, previous, last, total_count)
