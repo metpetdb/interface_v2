0427a2805e9ce8404093c2b375ab5a9fc9757f2c
Krishna Aradhi <krishna@aradhi.me>
2014-05-29 22:08:45 -0400

    Implement the samples list page

---
+++ utilities.py
@@ -0,0 +1,26 @@
+from flask import url_for
+
+def paginate_model(model_name, data, filters):
+    new_filters = dict(filters)
+    del new_filters['offset']
+    next, previous = None, None
+
+    if data.data['meta']['next']:
+        next_offset = int(filters['offset']) + 20
+        next = url_for(model_name) + '?' + 'offset={0}'.format(next_offset)
+        if new_filters:
+            next = next + '&' + urllib.urlencode(new_filters)
+
+    if data.data['meta']['previous']:
+        prev_offset = int(filters['offset']) - 20
+        previous = url_for(model_name) + '?' + 'offset={0}'.format(prev_offset)
+        if new_filters:
+            previous = previous + '&' + urllib.urlencode(new_filters)
+
+    total_count = data.data['meta']['total_count']
+    last = url_for(model_name) + '?' + 'offset={0}'.format(\
+                  total_count - total_count%20)
+    if new_filters:
+        last = last + '&' + urllib.urlencode(new_filters)
+
+    return (next, previous, last, total_count)
