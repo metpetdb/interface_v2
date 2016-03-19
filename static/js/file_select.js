b3d7f9b5c5c304c0faae051e010d97bf76dddc21
asdf <wennis@rpi.edu>
2016-03-19 11:00:56 -0400

    Change name of global url variable to void conflict

--- static/js/file_select.js
+++ static/js/file_select.js
@@ -1,9 +1,9 @@
 //Global variables
-var URL = null;
+var fileSelectURL = null;
 
 //Capture the URL from the user via a pop-up text box
 function GetFileURL() {
-    URL = prompt("Please enter the URL of the file you want to upload");
+    fileSelectURL = prompt("Please enter the URL of the file you want to upload");
     //Check if an X or check mark already exist. If one does, remove it.
     var OldImage = document.getElementById('symbol');
     if (OldImage) {
@@ -13,29 +13,29 @@ function GetFileURL() {
     var img = document.createElement("img");
     img.alt = "image";
     img.id = "symbol";
-    if (URL != null) {
-        if (!ValidateURL(URL)) {
-            //alert(URL + " is not a valid URL");
+    if (fileSelectURL != null) {
+        if (!ValidateURL(fileSelectURL)) {
+            //alert(fileSelectURL + " is not a valid URL");
             img.height="20";
             img.width="20";
             img.src = "../static/images/x.jpg";
-            URL = null;
+            fileSelectURL = null;
             
         }
         else {
             img.height="24";
             img.width="24";
             img.src = "../static/images/check.png";
-            document.getElementById('URLEntered').innerHTML = URL;
+            document.getElementById('URLEntered').innerHTML = fileSelectURL;
         }
         document.getElementById('CheckOrX').appendChild(img);
     }
 }
 
 //Validate the URL using regex
-function ValidateURL(URL) {
+function ValidateURL(fileSelectURL) {
     var Regex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
-    return Regex.test(URL);
+    return Regex.test(fileSelectURL);
 }
 
 //Submit the URL
@@ -50,13 +50,13 @@ function ParseFileForUpload() {
     else if (document.getElementById('Images').checked) {
         Checked = "Images";
     }
-    if (URL == null || Checked == null) {
+    if (fileSelectURL == null || Checked == null) {
         return;
     }
     //Send Checked and URL as JSON using POST
     var data = {};
     data["checked"] = Checked;
-    data["url"] = URL;
+    data["url"] = fileSelectURL;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/test", false);
     xhr.setRequestHeader("Content-type", "application/json");
